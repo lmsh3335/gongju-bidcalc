0fdb856f75743f5b4ae59c1d540523f2c1f304d4
lmsh3335 <lmsh3335@gmail.com>
2025-11-04 13:17:09 +0900

    fix package.json formatting

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BidCalculator from "./src/BidCalculator.jsx";
+import "./src/index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BidCalculator />
+  </React.StrictMode>
+);
