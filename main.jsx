0ca540f9418a4d51a76782b7623b0bf25d2ee98d
lmsh3335 <lmsh3335@gmail.com>
2025-11-04 12:54:54 +0900

    fix: correct component render

--- main.jsx
+++
@@ -1,1 +0,0 @@
-import React from 'react';import ReactDOM from 'react-dom/client';import BidCalculator from './src/BidCalculator.jsx';import './src/index.css';ReactDOM.createRoot(document.getElementById('root')).render(<BidCalculator />);
